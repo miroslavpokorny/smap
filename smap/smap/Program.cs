c34ea474319e13caceb29ae3cac2d7c93629322e
Miroslav Pokorný <miroslavpokorny94@gmail.com>
2018-12-29 19:46:25 +0100

    Refactor + file structure

--- smap/smap/Program.cs
+++ smap/smap/Program.cs
@@ -1,6 +1,7 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
 using System.Drawing;
-using System.Drawing.Imaging;
 using System.IO;
 using System.Text;
 using PdfSharp.Drawing;
@@ -15,7 +16,43 @@ namespace smap
         static void Main(string[] args)
         {
             Encoding.RegisterProvider(CodePagesEncodingProvider.Instance);
-            Console.WriteLine("Hello World!");
+
+            var bmpStream = CreateQrCode();
+            
+            CreatePdf(bmpStream, GenerateRandomString(2784).Split(58));
+
+            // ReadQrCode();
+        }
+
+        private static void CreatePdf(MemoryStream qrCode, IEnumerable<string> data)
+        {
+            var document = new PdfDocument();
+            document.Info.Title = "Binary data backup";
+
+            var page = document.AddPage();
+            var gfx = XGraphics.FromPdfPage(page);
+            var font = new XFont("Consolas", 12, XFontStyle.Regular);
+
+            // 57 x 48
+            var rowIndex = 0;
+            foreach (var rowData in data)
+            {
+                for (var i = 0; i < rowData.Length; i++)
+                {
+                    gfx.DrawString(rowData[i].ToString(), font, XBrushes.Black, 37 + i * 9, 30 + 57 + 14 + rowIndex * 15);                    
+                }
+                rowIndex++;
+            }
+            
+            var image = XImage.FromStream(qrCode);
+            // A4 width: 595 pt ; height: 842 pt 
+            gfx.DrawImage(image, 30, 30, 57, 57);
+
+            document.Save("first.pdf");
+        }
+
+        private static MemoryStream CreateQrCode()
+        {
             var metaData = new MetaData
             {
                 DataChunkSize = 63,
@@ -33,24 +70,11 @@ namespace smap
             var pixelData = barcodeWriter.Write(Encoding.UTF8.GetString(metaData.ToBytes()));
 
             var bmp = ImageHelper.PixelDataToBmp(pixelData, 10);
-            var bmpStream = new MemoryStream(bmp);
-            File.WriteAllBytes("FirstBmp.bmp", bmp);
-            
-//            var document = new PdfDocument();
-//            document.Info.Title = "Binary data backup";
-//
-//            var page = document.AddPage();
-//            var gfx = XGraphics.FromPdfPage(page);
-//            var font = new XFont("Consolas", 12, XFontStyle.Regular);
-//            
-//            gfx.DrawString("Hello world!", font, XBrushes.Black, new XRect(0, 0, page.Width, page.Height), XStringFormats.Center);
-//
-//            var image = XImage.FromStream(bmpStream);
-//            // A4 width: 595 pt ; height: 842 pt 
-//            gfx.DrawImage(image, 0,0, 250, 250);
-//            
-//            document.Save("first.pdf");
+            return new MemoryStream(bmp);
+        }
 
+        private static void ReadQrCode()
+        {
             var barcodeReader = new BarcodeReader();
             
             var bitmap = Image.FromFile("FirstBmp.bmp") as Bitmap;
@@ -61,6 +85,19 @@ namespace smap
             
             var readMetaData = new MetaData();
             readMetaData.Parse(Encoding.UTF8.GetBytes(result.Text));
+        }
+
+        private static string GenerateRandomString(int strLen)
+        {
+            var alphabet = Alphabet.Base32Alphabet.ToString();
+            var stringBuilder = new StringBuilder(strLen);
+            var random = new Random();
+            for (var i = 0; i < strLen; i++)
+            {
+                stringBuilder.Append(alphabet[random.Next(0, alphabet.Length)]);
+            }
+
+            return stringBuilder.ToString();
         }
     }
 }
---
+++ smap/smap/StringExtensions.cs
@@ -0,0 +1,14 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace smap
+{
+    public static class StringExtensions
+    {
+        public static IEnumerable<string> Split(this string str, int chunkSize)
+        {
+            return Enumerable.Range(0, (int) Math.Ceiling(str.Length / (double) chunkSize)).Select(i => str.Substring(i * chunkSize, (i+1) * chunkSize > str.Length ? str.Length % chunkSize : chunkSize));
+        }
+    }
+}
